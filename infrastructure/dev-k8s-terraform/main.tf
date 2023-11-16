29daf28361333db7a71c3557f31109437fba2857
hfz0106 <hfz0106@gmail.com>
2023-11-16 21:05:16 +0000

    added qa automation pipeline for dev

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -158,11 +158,7 @@ resource "aws_instance" "kube-master" {
     iam_instance_profile = aws_iam_instance_profile.petclinic-master-server-profile.name
     vpc_security_group_ids = [aws_security_group.petclinic-kube-master-sg.id, aws_security_group.petclinic-mutual-sg.id]
     key_name = "clarus"
-<<<<<<< HEAD
     subnet_id = "subnet-08487e88cdd7792c5"  # select own subnet_id of us-east-1a
-=======
-    subnet_id = "subnet-c41ba589"  # select own subnet_id of us-east-1a
->>>>>>> feature/msp-15
     availability_zone = "us-east-1a"
     tags = {
         Name = "kube-master"
@@ -178,11 +174,7 @@ resource "aws_instance" "worker-1" {
     instance_type = "t3a.medium"
     vpc_security_group_ids = [aws_security_group.petclinic-kube-worker-sg.id, aws_security_group.petclinic-mutual-sg.id]
     key_name = "clarus"
-<<<<<<< HEAD
     subnet_id = "subnet-08487e88cdd7792c5"  # select own subnet_id of us-east-1a
-=======
-    subnet_id = "subnet-c41ba589"  # select own subnet_id of us-east-1a
->>>>>>> feature/msp-15
     availability_zone = "us-east-1a"
     tags = {
         Name = "worker-1"
@@ -198,11 +190,7 @@ resource "aws_instance" "worker-2" {
     instance_type = "t3a.medium"
     vpc_security_group_ids = [aws_security_group.petclinic-kube-worker-sg.id, aws_security_group.petclinic-mutual-sg.id]
     key_name = "clarus"
-<<<<<<< HEAD
     subnet_id = "subnet-08487e88cdd7792c5"  # select own subnet_id of us-east-1a
-=======
-    subnet_id = "subnet-c41ba589"  # select own subnet_id of us-east-1a
->>>>>>> feature/msp-15
     availability_zone = "us-east-1a"
     tags = {
         Name = "worker-2"
@@ -226,7 +214,7 @@ output worker-1-ip {
 }
 
 output worker-2-ip {
-  value       = aws_instance.worker-2.public_ip 
+  value       = aws_instance.worker-2.public_ip
   sensitive   = false
   description = "public ip of the worker-2"
 }
